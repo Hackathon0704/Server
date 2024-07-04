84bb3a8491a42e804b5d9187cc1dc2076615ffbc
이승연 <dltmddus9082@naver.com>
2024-07-04 20:59:47 +0900

    Feat: Swagger 설정 추가

--- src/main/java/com/umc/dream/config/SwaggerConfig.java
+++ src/main/java/com/umc/dream/config/SwaggerConfig.java
@@ -1,10 +1,10 @@
 package com.umc.dream.config;
 
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,6 +29,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
         // Swagger UI 접속 후, 딱 한 번만 accessToken을 입력해주면 모든 API에 토큰 인증 작업이 적용됩니다.
         return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("https://dev.seungyeon.shop").description("Develop server"), new Server().url("http://localhost:8080").description("Local server")))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
