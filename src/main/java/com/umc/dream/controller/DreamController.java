338e8d12153137f364a1ff2b98699dfad34cb3f4
이현주 <lhjivy1024@gmail.com>
2024-07-05 05:21:41 +0900

    [feat/3] swagger 명세 추가

--- src/main/java/com/umc/dream/controller/DreamController.java
+++ src/main/java/com/umc/dream/controller/DreamController.java
@@ -43,6 +43,11 @@ public class DreamController {
     }
 
     @GetMapping("/diary/{user_id}/{diary_id}")
+    @Operation(summary = "내 꿈 일기 상세 조회", description = "내 꿈 일기 상세 조회 API입니다")
+    @Parameters({
+            @Parameter(name = "user_id", description = "자신의 pk"),
+            @Parameter(name = "dream_id", description = "꿈 일기의 pk")
+    })
     private ApiResponse<ViewDreamResponseDto> ViewDream(@RequestParam Long user_id, @RequestParam Long dream_id) {
         ViewDreamResponseDto viewDreamResponseDto = dreamService.viewDream(user_id, dream_id);
         return ApiResponse.onSuccess(viewDreamResponseDto);
